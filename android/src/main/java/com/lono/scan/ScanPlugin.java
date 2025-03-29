8cd95d350fc027571315b837c2f657aadf16d28e
Lonokokkok <lono@kokkok.la>
2025-03-29 15:09:01 +0700

    update

--- android/src/main/java/com/lono/scan/ScanPlugin.java
+++ android/src/main/java/com/lono/scan/ScanPlugin.java
@@ -1,31 +1,16 @@
 package com.lono.scan;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.common.GlobalHistogramBinarizer;
 import com.google.zxing.common.HybridBinarizer;
-import com.journeyapps.barcodescanner.CaptureActivity;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
-
-import androidx.annotation.NonNull;
 
 import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -35,110 +20,119 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import androidx.annotation.NonNull;  // <-- Add this import statement
 
 import static android.content.Context.VIBRATOR_SERVICE;
 
 /** ScanPlugin */
 public class ScanPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  private MethodChannel channel;
-  private Activity activity;
-  private FlutterPluginBinding flutterPluginBinding;
-  private Result _result;
-  private QrCodeAsyncTask task;
+    private MethodChannel channel;
+    private Activity activity;
+    private FlutterPluginBinding flutterPluginBinding;
+    private Result _result;
+    private QrCodeAsyncTask task;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    this.flutterPluginBinding = flutterPluginBinding;
-  }
-
-  private void configChannel(ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "lono/scan");
-    channel.setMethodCallHandler(this);
-    flutterPluginBinding.getPlatformViewRegistry()
-            .registerViewFactory("lono/scan_view", new ScanViewFactory(
-                    flutterPluginBinding.getBinaryMessenger(),
-                    flutterPluginBinding.getApplicationContext(),
-                    activity,
-                    binding
-            ));
-  }
-
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    configChannel(binding);
-  }
-
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    configChannel(binding);
-  }
-
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-  }
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    this.flutterPluginBinding = null;
-  }
-
-  @Override
-  public void onDetachedFromActivity() {
-    activity = null;
-    channel.setMethodCallHandler(null);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    _result = result;
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method.equals("parse")) {
-      String path = (String) call.arguments;
-      task = new QrCodeAsyncTask(this, path);
-      task.execute(path);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        this.flutterPluginBinding = flutterPluginBinding;
+        // Set up MethodChannel
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "lono/scan");
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  /**
-   * AsyncTask Static inner class to prevent memory leaks
-   */
-  static class QrCodeAsyncTask extends AsyncTask<String, Integer, String> {
-    private final WeakReference<ScanPlugin> mWeakReference;
-    private final String path;
-
-    public QrCodeAsyncTask(ScanPlugin plugin, String path) {
-      mWeakReference = new WeakReference<>(plugin);
-      this.path = path;
+    private void configChannel(ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        flutterPluginBinding.getPlatformViewRegistry()
+                .registerViewFactory("lono/scan_view", new ScanViewFactory(
+                        flutterPluginBinding.getBinaryMessenger(),
+                        flutterPluginBinding.getApplicationContext(),
+                        activity,
+                        binding
+                ));
     }
 
     @Override
-    protected String doInBackground(String... strings) {
-      // Parsing QR code/barcode
-      return QRCodeDecoder.decodeQRCode(mWeakReference.get().flutterPluginBinding.getApplicationContext(), path);
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        configChannel(binding);
     }
 
     @Override
-    protected void onPostExecute(String s) {
-      super.onPostExecute(s);
-      //Identify the QR code/barcode in the image, the content is s
-      ScanPlugin plugin = (ScanPlugin) mWeakReference.get();
-      plugin._result.success(s);
-      plugin.task.cancel(true);
-      plugin.task = null;
-      if (s!=null) {
-        Vibrator myVib = (Vibrator) plugin.flutterPluginBinding.getApplicationContext().getSystemService(VIBRATOR_SERVICE);
-        if (myVib != null) {
-          if (Build.VERSION.SDK_INT >= 26) {
-            myVib.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-            myVib.vibrate(50);
-          }
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        configChannel(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        this.flutterPluginBinding = null;
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        _result = result;
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("parse")) {
+            String path = (String) call.arguments;
+            task = new QrCodeAsyncTask(this, path);
+            task.execute(path);
+        } else {
+            result.notImplemented();
         }
-      }
     }
-  }
+
+    /**
+     * AsyncTask Static inner class to prevent memory leaks
+     */
+    static class QrCodeAsyncTask extends AsyncTask<String, Integer, String> {
+        private final WeakReference<ScanPlugin> mWeakReference;
+        private final String path;
+
+        public QrCodeAsyncTask(ScanPlugin plugin, String path) {
+            mWeakReference = new WeakReference<>(plugin);
+            this.path = path;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            // QR code decoding logic here
+            return QRCodeDecoder.decodeQRCode(mWeakReference.get().flutterPluginBinding.getApplicationContext(), path);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            // Call the Flutter result with the decoded QR code data
+            ScanPlugin plugin = mWeakReference.get();
+            if (plugin != null) {
+                plugin._result.success(result);
+
+                // Handle QR code vibration feedback
+                if (result != null) {
+                    Vibrator vibrator = (Vibrator) plugin.flutterPluginBinding.getApplicationContext().getSystemService(VIBRATOR_SERVICE);
+                    if (vibrator != null) {
+                        if (Build.VERSION.SDK_INT >= 26) {
+                            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+                        } else {
+                            vibrator.vibrate(50);
+                        }
+                    }
+                }
+
+                // Clean up
+                plugin.task.cancel(true);
+                plugin.task = null;
+            }
+        }
+    }
 }
